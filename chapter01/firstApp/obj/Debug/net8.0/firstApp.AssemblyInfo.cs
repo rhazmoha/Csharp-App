f964a443c33b2d58772b7dec5240196d30fc1e73
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-13 12:32:43 +0000

    first

--- chapter01/firstApp/obj/Debug/net8.0/firstApp.AssemblyInfo.cs
+++ chapter01/firstApp/obj/Debug/net8.0/firstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05ad616cd56c9fe8964db52e5066a2367ddb80b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4077a58a7835fb7f2f9084f9b219584f280d321b")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
