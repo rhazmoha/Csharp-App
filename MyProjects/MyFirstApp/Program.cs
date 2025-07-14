d10134e50a28f24667ae5515b582a8d73df95b26
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-14 13:22:51 +0000

    first

---
+++ MyProjects/MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml;
+using static System.Console;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var x = 10.5;
+        WriteLine("Hello, World!");
+        var xml1 = new XmlDocument();
+        
+    }
+}
