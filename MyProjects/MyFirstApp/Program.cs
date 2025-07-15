69fa8bae0889cd0ed3807926f08058d155d8c7f3
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-15 20:04:09 +0000

    first

--- MyProjects/MyFirstApp/Program.cs
+++ MyProjects/MyFirstApp/Program.cs
@@ -23,16 +23,11 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        string applesText = "Apples";
-        int applesCount = 1234;
-        string bananasText = "Bananas";
-        int bananasCount = 56789;
-
-        WriteLine();
-        WriteLine($"{"Name",-10}|{"Count",6}");
-        WriteLine(new string('-', 20));
-        WriteLine($"{applesText,-10}|{applesCount,6:N0}");
-        WriteLine($"{bananasText,-10}|{bananasCount,6:N0}");
+        double d = 0.6;
+        int i =  65025;
+        WriteLine($"{d:e}");
+        
+        
 
 
 
