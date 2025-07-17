a5c18606bf43a047a595d91ba55eed14d951afc7
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-17 14:29:26 +0000

    commit

--- chapter03/operators/Program.cs
+++ chapter03/operators/Program.cs
@@ -2,6 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        
+        #region Exploring unary operators
+
+        int a = 3, b;
+
+        b = a++;
+        WriteLine($"a is {a}, b is {b}");
+
+
+        #endregion
     }
 }
