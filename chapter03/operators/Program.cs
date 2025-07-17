6029e42de4c698ca179fa7d0d46ae34180c42da5
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-17 14:36:03 +0000

    commit

--- chapter03/operators/Program.cs
+++ chapter03/operators/Program.cs
@@ -9,6 +9,9 @@
         b = a++;
         WriteLine($"a is {a}, b is {b}");
 
+        int c = 3;
+        int d = ++c; // Prefix means increment c before assigning it.
+        WriteLine($"c is {c}, d is {d}");
 
         #endregion
     }
