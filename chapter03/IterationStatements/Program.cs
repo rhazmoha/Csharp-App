cbf1ea122da13837468ebd5bc96b9219024153bb
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-19 16:03:08 +0000

    commit

--- chapter03/IterationStatements/Program.cs
+++ chapter03/IterationStatements/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+using static System.Console;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        for (long y = 0; y < long.MaxValue; y++)
+        {
+
+        }
+        var time = stopWatch.Elapsed;
+        WriteLine($"end of program in {time.Milliseconds} ms");
+        
     }
 }
--- chapter03/IterationStatements/obj/Debug/net8.0/IterationStatements.AssemblyInfo.cs
+++ chapter03/IterationStatements/obj/Debug/net8.0/IterationStatements.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IterationStatements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4cdb8ff36a44c394eeaf411a3428626908c6358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ca731df5ba1da71ebad3f5f76c4e7b5ee81422")]
 [assembly: System.Reflection.AssemblyProductAttribute("IterationStatements")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IterationStatements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
