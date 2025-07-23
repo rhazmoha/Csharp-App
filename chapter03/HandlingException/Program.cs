293fc19a5e8c583cda39c6fa25114f485714b280
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-23 14:36:58 +0000

    commit

--- chapter03/HandlingException/Program.cs
+++ chapter03/HandlingException/Program.cs
@@ -1,7 +1,30 @@
-﻿internal class Program
+﻿using static System.Console;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        WriteLine("Before parsing");
+        Write("What is your age? ");
+        string? input = ReadLine();
+
+        /* if (input is null)
+        {
+            WriteLine("You did not enter a value so the app has ended.");
+            return; // Exit the app.
+        } */
+
+        try
+        {
+            int age = int.Parse(input!);
+            WriteLine($"You are {age} years old.");
+        }
+        catch
+        {
+            
+        }
+        WriteLine("After parsing");
+
+
     }
 }
