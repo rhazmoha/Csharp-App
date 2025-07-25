04eedebf47e687ca16f8f0ed98f7b48c1aee78d2
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-07-25 20:14:19 +0000

    commit

--- Chapter04/TopLevelFunctions/Program.Functions.cs
+++ Chapter04/TopLevelFunctions/Program.Functions.cs
@@ -2,6 +2,65 @@ using static System.Console;
 using System.Globalization;
 partial class Program
 {
+
+    static void RunFactorial()
+    {
+        for (int i = 1; i <= 202; i += 100)
+        {
+            try
+            {
+                WriteLine($"{i}! = {Factorial(i):N0}");
+
+            }
+            catch (OverflowException)
+            {
+                WriteLine($"{i}! is too big for a 32-bit integer.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{i}! throws {ex.GetType()}: {ex.Message}");
+            }
+        }
+
+    }
+    static int FactorialLoop(int number)
+    {
+        int result = 1;
+
+        while (number > 0)
+        {
+            result *= number;
+            number--;
+        }
+
+        return result;
+
+    }
+
+
+
+    static double Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number), $"The factorial function is defined for non-negative integers only. Input: {number}");
+
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            checked
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+
+
+    }
+
     static void RunCardinalToOrdinal()
     {
         for (uint number = 1; number <= 1500; number++)
@@ -12,6 +71,13 @@ partial class Program
 
         WriteLine();
     }
+
+    /// <summary>
+    /// Pass a 32-bit unsigned integer and it will be converted into its
+    ///ordinal equivalent
+    /// </summary>
+    /// <param name="number">Number as a cardinal value e.g. 1, 2, 3, and so on.</param>
+    /// <returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on.</returns>
     static string CardinalToOrdinal(uint number)
     {
         uint lastTwoDigits = number % 100;
--- Chapter04/TopLevelFunctions/Program.cs
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -2,7 +2,9 @@
 
 
 
-RunCardinalToOrdinal();
+CardinalToOrdinal()
+
+
 
 
 
