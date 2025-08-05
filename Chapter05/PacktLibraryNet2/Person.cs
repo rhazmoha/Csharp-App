f85733faf94aaacadf5e9c5c04de8b3f0bbc6d30
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-08-05 19:40:51 +0000

    commit

---
+++ Chapter05/PacktLibraryNet2/Person.cs
@@ -0,0 +1,11 @@
+namespace Packt.Shared;
+
+public class Person : object
+{
+    public Person()
+    {
+        
+        
+    }
+
+}
---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Packt.Shared;
+using Tx = Texas;
+using Env = System.Environment;
+
+ConfigureConsole();
+
+Person bob = new();
+
+WriteLine(bob);
+
+WriteLine(Env.OSVersion);
