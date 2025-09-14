c3000c907e72e62770a6ee8b6fc4e2603feda6f3
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-09-14 15:06:39 +0000

    commit

--- CsharpTuto/MyTuto/Program.cs
+++ CsharpTuto/MyTuto/Program.cs
@@ -22,15 +22,33 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var customer = new Customer() { Id = 2, Name = "jack" };
-       // customer.Id = 1;
-       // customer.Name = "john";
         
-        System.Console.WriteLine(customer.Name);
+        var result = int.TryParse("569", out int number);
+      string x =  result ? number.ToString() : "failed";
 
-        
-        
+    }
 
+    static void UseParams()
+    {
+        var calc = new Calculator();
+        System.Console.WriteLine(calc.Add(new int[] {1,5,6,8}));
+    }
 
+    static void UsePoints()
+    {
+        try
+        {
+            var point = new Point(10, 20);
+            point.Move(null);
+
+            System.Console.WriteLine("point is at ({0}, {1})", point.X, point.Y);
+
+            point.Move(100, 200);
+            System.Console.WriteLine("point is at ({0}, {1})", point.X, point.Y);
+        }
+        catch (System.Exception)
+        {
+            System.Console.WriteLine("an unexpected error occured");
+        }
     }
 }
--- CsharpTuto/MyTuto/obj/Debug/net8.0/MyTuto.AssemblyInfo.cs
+++ CsharpTuto/MyTuto/obj/Debug/net8.0/MyTuto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTuto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aea8e2696319b165d70513eb35cc1e03537d1cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686255d32cd30c00f042e4083e99ef2bc3d16dbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTuto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTuto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
