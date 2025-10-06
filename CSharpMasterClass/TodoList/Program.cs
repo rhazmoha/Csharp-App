4859616de7f99f6e847d2f90a289ee1774bef806
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-10-06 13:06:37 +0000

    commit

--- CSharpMasterClass/TodoList/Program.cs
+++ CSharpMasterClass/TodoList/Program.cs
@@ -1,6 +1,13 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Mohammed");
+Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
 System.Console.ReadKey();
 
-
+int a, b, c;
+a = b = c = 10;
+System.Console.WriteLine($"{a}");
--- CSharpMasterClass/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ CSharpMasterClass/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa2f5a0c4b452afa1255d577ded959e2f2e89b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+addbaa21e01a14da010ed7cfff9696271332d168")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
