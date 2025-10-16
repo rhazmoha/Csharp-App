11b54770f2d0e089240fb7309f528ddb0a27f6f2
rhazmoha <112493525+rhazmoha@users.noreply.github.com>
2025-10-16 14:27:37 +0000

    commit

--- CSharpMasterClass/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ CSharpMasterClass/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0694f82b042ac72ddee8277a8d8b9eb776a4a6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc2419ee90dab91debb6e3f94b72c20d2a4fef5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CSharpMasterClass/TodoList/Program.cs
+++ CSharpMasterClass/TodoList/Program.cs
@@ -1,115 +1,73 @@
-﻿
-var nums = new int[,]
+﻿string userChoice;
+var listTodo = new List<string>()
 {
-    {3, 5},
-{ -1, 12},
-{4,0}
+    "make a cake",
+"watch TV"
 };
 
-System.Console.WriteLine(FindMax(nums));
-return;
 
-System.Console.WriteLine("Enter a word");
-var userInput = Console.ReadLine();
-
-while (userInput.Length < 15)
+do
 {
-    userInput = userInput + 'a';
-    System.Console.WriteLine(userInput);
-}
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
 
-System.Console.WriteLine("the loop is finished");
+    userChoice = Console.ReadLine().ToUpper();
 
-
-Console.WriteLine("Hello!");
-Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
-
-string userChoice = Console.ReadLine();
-
-
-switch (userChoice)
-{
-    case "S":
-        PrintSelectedOption("See All TODOs");
-        break;
-
-    case "A":
-        PrintSelectedOption("Add a TODO");
-        break;
-
-    case "R":
-        PrintSelectedOption("Remove a TODO");
-        break;
-
-    case "E":
-        PrintSelectedOption("Exit");
-        break;
-
-    default:
-        System.Console.WriteLine("Invalid choice");
-        break;
-
-}
-
-
-
-char ConvertPointsToGrade(int points)
-{
-
-    return points switch
+    switch (userChoice)
     {
-        >= 90 => 'A',
-        >= 80 => 'B',
-        >= 70 => 'C',
-        _ => 'D'
-    };
+        case "S":
+
+            if (listTodo.Count == 0)
+            {
+                System.Console.WriteLine("No TODOs have been added yet.");
+
+            }
+            else
+            {
+                for (int i = 0; i < listTodo.Count; i++)
+                {
+                    System.Console.WriteLine($"{i + 1}. {listTodo[i]}");
+                }
+
+            }
+
+            System.Console.WriteLine();
+
+            break;
+
+        case "A":
+            break;
+
+        case "R":
+            break;
+
+        case "E":
+            break;
+        default:
+            System.Console.WriteLine("Incorrect input");
+            break;
+    }
 
 
-}
+} while (userChoice != "E");
 
-bool IsLong(string input)
-{
-    return input.Length > 10;
-}
 
-void PrintSelectedOption(string selectedOption)
-{
-    System.Console.WriteLine("Selected Option: " + selectedOption);
-}
+
+
+
+
+
+
+
+
 
 
 #region Exercises
 
-int FindMax(int[,] array)
-{
-    int rows = array.GetLength(0);
-    int cols = array.GetLength(1);
-
-    if (rows == 0 || cols == 0)
-    {
-        return -1;
-    }
-
-    int max = array[0, 0];
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            if (max < array[i, j])
-            {
-                max = array[i, j];
-            }
-        }
-    }
-
-    return max;
-
-}
 
 
 #endregion
